8f3d9b65ca54935ba354418605ee5df868b99146
Mayank <mayankbansal125@gmail.com>
2023-10-29 21:36:53 +0530

    Resolve service worker error

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,16 +2,9 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
 importScripts(
   "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
 );
+import { envConfig } from "./env-config";
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
-};
+const firebaseConfig = { envConfig };
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();

8d3f15f4bfc921a7daa80cae4b2d62e6ad7e40cd
Saakshi Raut <saakshiraut28@gmail.com>
2023-11-11 22:33:46 +0530

    Create a portal

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        josefin: ["Josefin Sans", "sans-serif"],
+        title: ["Tourney", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
